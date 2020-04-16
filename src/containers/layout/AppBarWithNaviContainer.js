6baee0f572ceb841b6026bbe64489a0e371139ce
JunL <gksdmltk02@gmail.com>
2020-04-16 14:10:27 +0900

    L

--- src/containers/layout/AppBarWithNaviContainer.js
+++ src/containers/layout/AppBarWithNaviContainer.js
@@ -7,30 +7,30 @@ import { withRouter } from 'react-router-dom';
 const AppBarWithNaviContainer = ({ history, children }) => {
 	const { data, error } = useQuery(CHECK);
 
-	useEffect(() => {
-		if (data) {
-			const { check } = data;
-			try {
-				const exist = localStorage.getItem('user');
-				if (!exist) {
-					localStorage.setItem('user', JSON.stringify(check));
-				}
-			} catch (e) {
-				console.log(e);
-			}
-		}
-		if (error) {
-			try {
-				let error_ = error.graphQLErrors[0].extensions.code;
-				if (error_ === 'unauthorized') {
-					localStorage.removeItem('user');
-					history.push('/auth/login');
-				}
-			} catch (e) {
-				console.log(e);
-			}
-		}
-	}, [data, error, history]);
+	// useEffect(() => {
+	// 	if (data) {
+	// 		const { check } = data;
+	// 		try {
+	// 			const exist = localStorage.getItem('user');
+	// 			if (!exist) {
+	// 				localStorage.setItem('user', JSON.stringify(check));
+	// 			}
+	// 		} catch (e) {
+	// 			console.log(e);
+	// 		}
+	// 	}
+	// 	if (error) {
+	// 		try {
+	// 			let error_ = error.graphQLErrors[0].extensions.code;
+	// 			if (error_ === 'unauthorized') {
+	// 				localStorage.removeItem('user');
+	// 				history.push('/auth/login');
+	// 			}
+	// 		} catch (e) {
+	// 			console.log(e);
+	// 		}
+	// 	}
+	// }, [data, error, history]);
 
 	return <AppBarWithNavi children={children}></AppBarWithNavi>;
 };
